a26008cf651aeb1279827164434d13ecad363857
Bradley Falzon <brad@teambrad.net>
2016-05-28 21:57:34 +0930

    Cleanup debug, add flags, readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Overview
+
+This is a toy proof of concept of a UDP load balancer which duplicates requests to all backends and responds to the client with the first response it receives (therefore the fastest).
+
+- Configure listening port with `listen` flag, for example `-listen 127.0.0.1:999`
+- Configure backends with the `-backends` flag, providing a comma separated list, such as `-backends 127.0.0.1:123,127.0.0.1:567`
+
+This is essentially the first iteration, and may contain bugs and performance issues. There are further optimisations, as each request creates a new listening socket for the backend response - this causes overhead (and syscalls), if the UDP messages could contain the IP and port, or other identifier which could be used to lookup where to send the responses to, a listening socket could be created at startup instead of at each request/response.
+
+```
+$ go get github.com/bradleyfalzon/udp-proxy
+$ udp-proxy -listen :1234 -backends 10.0.0.1:1234,10.0.0.2:1234
+```
--- main.go
+++ main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net"
+	"os"
 	"strings"
 )
 
-var backends []*net.UDPAddr
+var (
+	backends []*net.UDPAddr
+)
 
 func main() {
+	flagListen := flag.String("listen", "", "host:port of UDP address to listen on, use :port for all interfaces")
+	flagBackends := flag.String("backends", "", "CSV of host:port backends")
+	flag.Parse()
 
-	log.Println("Resolving backends")
+	if *flagBackends == "" || *flagListen == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	log.Println("Backends:", *flagBackends)
 
 	// Resolve backends
-	for _, be := range strings.Split("127.0.0.1:44120,127.0.0.1:44121", ",") {
+	for _, be := range strings.Split(*flagBackends, ",") {
 		beAddr, err := net.ResolveUDPAddr("udp", be)
 		if err != nil {
 			log.Fatalf("Cannot resolve backend addr %v: %v", be, err)
@@ -21,10 +33,10 @@ func main() {
 		backends = append(backends, beAddr)
 	}
 
-	log.Println("Listening to UDP")
+	log.Println("Listening to UDP:", *flagListen)
 
 	// Listen to UDP
-	addr, err := net.ResolveUDPAddr("udp", ":1234")
+	addr, err := net.ResolveUDPAddr("udp", *flagListen)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -44,30 +56,22 @@ func main() {
 			log.Println("Error reading from udp on server listen loop: ", err)
 			continue
 		}
-
-		//log.Printf("received %v from %v", string(buf[:n]), addr)
-
 		go handlePacket(buf[:n], addr, conn)
 	}
 }
 
 func handlePacket(msg []byte, raddr *net.UDPAddr, conn *net.UDPConn) {
-
-	// Create the listen addr
+	// Listen on port for respones
 	laddr, err := net.ResolveUDPAddr("udp", ":0")
 	if err != nil {
 		log.Fatalf("Could not resolve listen addr: %v", err)
 	}
-
-	// Listen on port for respones
 	lconn, err := net.ListenUDP("udp", laddr)
 	if err != nil {
 		log.Fatal("Could not listen for backend repsonse:", err)
 	}
 
 	go func(raddr *net.UDPAddr, conn, lconn *net.UDPConn) {
-		//log.Println("Backend listen loop ready")
-
 		rbuf := make([]byte, 1500)
 
 		// Receive the response
@@ -76,19 +80,15 @@ func handlePacket(msg []byte, raddr *net.UDPAddr, conn *net.UDPConn) {
 			log.Fatalln("Could not read from UDP in be listen loop:", err)
 		}
 
-		//log.Printf("received %v from %v", string(rbuf[:n]), addr)
-
 		// Send response back to client
 		conn.WriteToUDP(rbuf[:n], raddr)
 
-		// Stop listening for other response
-		lconn.Close() // TODO pass reference
-		return
+		// Stop listening for other responses
+		lconn.Close()
 	}(raddr, conn, lconn)
 
 	// Send the request to all backends
 	for _, be := range backends {
-		//log.Println("Sending to backend:", be)
 		lconn.WriteToUDP(msg, be)
 	}
 }
